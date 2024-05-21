6abf38c6e564a06c9eb0054853995703b9f24d42
mmackowsky <maciostah@gmail.com>
2024-05-21 21:35:39 +0200

    feat(models): update

--- src/models.py
+++ src/models.py
@@ -6,9 +6,15 @@ from database import Base
 
 class Device(Base):
     __tablename__ = "devices"
+    __table_args__ = {"extend_existing": True}
 
     id = Column(Integer, primary_key=True, nullable=False)
     user = Column(Integer, nullable=False)
     name = Column(String, nullable=False)
     producer = Column(String, nullable=False)
     description = Column(String, nullable=False)
+
+    def __repr__(self):
+        return (
+            f"{self.id}, {self.user}, {self.name}, {self.producer}, {self.description}"
+        )
